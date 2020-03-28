1093ed14e4f425bb3c562a0ff5f318b9770fcec4
Alex <alex.p4pp@gmail.com>
2020-03-28 17:32:31 +0100

    Half implemented Unstable's methods

--- src/projlab/Unstable.java
+++ src/projlab/Unstable.java
@@ -1,16 +1,30 @@
 package projlab;
 
+import java.util.ArrayList;
+
 public class Unstable extends Field {
 	private int maxPlayer;
 	private boolean flipped;
 	private String objName;
 	
 	public void Flip() {
-		
+		//TODO: print
+		flipped = !flipped;
+
+		if (flipped) {
+			for (Player p : GetPlayers()) {
+				p.SetTempToOne();
+				p.SetDrowning(1);
+				p.EndRound();
+			}
+		}
 	}
 	
 	public void AddPlayer(Player p) {
-		
+		super.AddPlayer(p); // should include the method call print
+		//TODO: flipped case, does Field.AddPlayer(p) need SetField?
+		if (GetPlayers().size() > maxPlayer)
+			Flip();
 	}
 	
 	public void RemovePlayer(Player p) {
