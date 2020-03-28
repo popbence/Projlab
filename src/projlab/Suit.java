5815ac2bad1fbe3e703f1c99134c8586dcb1f58e
Alex <alex.p4pp@gmail.com>
2020-03-28 15:29:43 +0100

    Suit prints methodCall info

--- src/projlab/Suit.java
+++ src/projlab/Suit.java
@@ -2,6 +2,10 @@ package projlab;
 
 public class Suit extends Item{
 	public void Use() {
+
+		String methodCall = objName + "." + Main.GetMethodName() + "()";
+		System.out.println(methodCall);
+
 		GetPlayer().SetDrowning(-1);
 	}
 }
