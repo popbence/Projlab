c61ef45e9093522028d31808a720c9320dc6ec80
Alex <alex.p4pp@gmail.com>
2020-03-28 15:16:24 +0100

    Implemented a function which returns the name of the caller method

--- src/projlab/Main.java
+++ src/projlab/Main.java
@@ -1,5 +1,13 @@
 package projlab;
 
 public class Main {
-	
+
+	public static String GetMethodName(){
+		/*
+		Returns the name of the method where this function was called.
+		getStackTrace()[0] -> getStackTrace, getStackTrace()[1] -> GetMethodName, getStackTrace()[2] -> caller
+		Was tested on the virtual machine the program's supposed to run on.
+		 */
+		return Thread.currentThread().getStackTrace()[2].getMethodName();
+	}
 }
