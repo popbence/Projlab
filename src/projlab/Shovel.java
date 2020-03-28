f003bb45c4354c25b46eb1d68d8f214f73a2213d
Alex <alex.p4pp@gmail.com>
2020-03-28 15:42:59 +0100

    Shovel's Use method implemented

--- src/projlab/Shovel.java
+++ src/projlab/Shovel.java
@@ -2,6 +2,10 @@ package projlab;
 
 public class Shovel extends Item{
 	public void Use() {
-		
+		String methodCall = objName + "." + Main.GetMethodName() + "()";
+		System.out.println(methodCall);
+
+		Field f = GetPlayer().GetField();
+		f.AddSnow(-2);
 	}
 }
