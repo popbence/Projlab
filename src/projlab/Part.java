e5b0118c82254ab3b48135175bb2abaa4fa39e7f
Zoli <zoli.v.97@gmail.com>
2020-03-28 14:16:02 +0100

    methods implemented for Part, Player, Item

--- src/projlab/Part.java
+++ src/projlab/Part.java
@@ -1,9 +1,27 @@
 package projlab;
 
+import java.util.ArrayList;
+
 public class Part extends Item {
 	private int type;
 	
 	public void Use() {
-		
+		System.out.println(objName + ".Use()");
+		Player p = this.GetPlayer().
+		Field f = p.GetField();
+		ArrayList<Player> playersOnField = new ArrayList<Player>();
+		playersOnField = f.GetPlayers();
+		ArrayList<Player> allPlayers = new ArrayList<Player>();
+		allPlayers = Controller.GetPlayers();
+		playersOnField.equals(allPlayers);
+		ArrayList<Item> items = p.GetItems();
+		int j = 0;
+		for (Item i : items) {
+			if(i instanceof Part) {
+				j++;
+			}
+		}
+		if(j == 3)
+			Controller.Win();
 	}
 }
