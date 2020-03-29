5037b13acd80cf23940e94ab2824e4a926d7cfd3
Alex <alex.p4pp@gmail.com>
2020-03-29 13:33:11 +0200

    Minor fix in Unstable.AddPlayer

--- src/projlab/Unstable.java
+++ src/projlab/Unstable.java
@@ -22,12 +22,16 @@ public class Unstable extends Field {
 	public void AddPlayer(Player p) {
 		super.AddPlayer(p); // should include the method call print
 		//TODO: flipped case, does Field.AddPlayer(p) need SetField?
-		if (GetPlayers().size() > maxPlayer)
+		if (flipped){
+			p.SetTempToOne();
+			p.SetDrowning(1);
+			p.EndRound();
+		} else if (GetPlayers().size() > maxPlayer)
 			Flip();
 	}
 	
 	public void RemovePlayer(Player p) {
-		
+		// TODO
 	}
 	
 	public int GetMaxPlayer() {
