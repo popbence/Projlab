c5efee179188f605912542317bd59174d537091f
Alex <alex.p4pp@gmail.com>
2020-03-29 23:22:12 +0200

    Rope.Use hibás implementációjának javítása

--- src/projlab/Rope.java
+++ src/projlab/Rope.java
@@ -15,13 +15,17 @@ public class Rope extends Item{ // TODO: Comments
 				// Ide az�rt kellett egy if, mert lehet null az egyik szomsz�d
 				if(fi != null){
 					ArrayList<Player> players = fi.GetPlayers();
+					ArrayList<Player> toRemove = new ArrayList<Player>();
 	
 					for (Player p : players) {
-						if (p.GetDrowning() == 1){
-							fi.RemovePlayer(p); //A kimentett jatekosoknak 0-ra kellene allitani a drowning erteket
-							p.SetDrowning(0);
-							f1.AddPlayer(p);
+						if (p.GetDrowning() == 1) {
+							toRemove.add(p);
 						}
+					}
+					for (Player p : toRemove){
+						fi.RemovePlayer(p); //A kimentett jatekosoknak 0-ra kellene allitani a drowning erteket
+						p.SetDrowning(0);
+						f1.AddPlayer(p);
 					}
 				}
 				i++;
